4b5ae208b430cc836dc255affcac8df061cfde2f
storm-sergey <st.sergey.a@gmail.com>
2022-01-09 10:13:20 +0300

    add the get local desktop address

--- lib/UserCredentials.cs
+++ lib/UserCredentials.cs
@@ -4,13 +4,18 @@ namespace AdminHelper.lib
 {
     public static class UserCredentials
     {
-        public static string UserName { get => WindowsIdentity.GetCurrent().Name; }
+        public static string UserName { get => WindowsIdentity.GetCurrent().Name.Split('\\')[1]; }
         public static string RuningAs { get => System.Environment.UserName; }
         public static string MachineName { get => System.Environment.MachineName; }
         public static string ComputerName { get => MachineName; }
         public static string LocationPrefix { get => MachineName.Split('-')[0]; }
         public static string AppDataLocal { get => $@"D:\Users\{UserName}\AppData\Local"; }
-
+        public static string LocalDesktop
+        { 
+            get => Files.CheckDirectoryExists($@"D:\Users\{UserName}\Desktop")
+                ? $@"D:\Users\{UserName}\Desktop"
+                : $@"D:\Users\{UserName}\Рабочий стол";
+        }
         //NetworkCredential myCredentials = new NetworkCredential(String.Format("{0}\\{1}", DOMAIN, userName), password);
     }
 }
