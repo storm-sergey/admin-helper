5dcfe88bd29d0d035842efd9e5a783395956739b
storm-sergey <st.sergey.a@gmail.com>
2022-03-06 10:40:08 +0300

    ref: codestyle

--- Globals.cs
+++ Globals.cs
@@ -17,13 +17,19 @@ namespace AdminHelper
         public static readonly string TICKETS_ENDPOINT = null;
         public static readonly string PRINT_SERVERS_ENDPOINT = null;
         public static readonly string PRINTERS_ENDPOINT = null;
+        public static readonly string SUBNETS_ENDPOINT = null;
+        public static readonly string LOG_ENDPOINT = null;
         #endregion
 
         #region RolfShare
         public static readonly string SHARE = @"\\1\IT_Distrib\";
-        public static readonly string TICKETS_FILE = $@"{SHARE}Ticket_templates.json";
-        public static readonly string PRINT_SERVERS_FILE = $@"{SHARE}Print_servers.json";
-        public static readonly string PRINTERS_FILE = $@"{SHARE}Printers_rooms.json";
+        public static readonly string SHARE_APP = $@"{SHARE}AdminHelper\";
+        public static readonly string CONFIG = $@"{SHARE_APP}config\";
+
+        public static readonly string SUBNETS_FILE = $@"{CONFIG}Subnets.json";
+        public static readonly string TICKETS_FILE = $@"{CONFIG}Ticket_templates.json";
+        public static readonly string PRINT_SERVERS_FILE = $@"{CONFIG}Print_servers.json";
+        public static readonly string PRINTERS_FILE = $@"{CONFIG}Printers_rooms.json";
         #endregion
 
         #region App
