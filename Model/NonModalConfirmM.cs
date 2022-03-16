46bb7611f741fc34159af852129688e7751c1993
storm-sergey <st.sergey.a@gmail.com>
2022-03-16 19:41:59 +0300

    Create non modal confirm window

---
+++ Model/NonModalConfirmM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AdminHelper.Model
+{
+    public class NonModalConfirmM
+    {
+        private bool AcceptanceHasBeenMade;
+        private bool isAccepted;
+        public string Title;
+        public string Question;
+        
+        public NonModalConfirmM(string question, string title)
+        {
+            Question = question;
+            Title = title;
+            AcceptanceHasBeenMade = false;
+            isAccepted = false;
+        }
+
+        public bool IsAccepted
+        {
+            get
+            {
+                if (AcceptanceHasBeenMade) return isAccepted;
+                else throw new Exception("NonModalConfirmM has no been made accept but an acceptance is required");
+            }
+            set { isAccepted = value; }
+        }
+
+        public void Accept()
+        {
+            AcceptanceHasBeenMade = true;
+            IsAccepted = true;
+        }
+
+        public void Cancel()
+        {
+            AcceptanceHasBeenMade = true;
+            IsAccepted = false;
+        }
+    }
+}
---
+++ ViewModel/NonModalConfirmVM.cs
@@ -0,0 +1,31 @@
+﻿using AdminHelper.Model;
+
+namespace AdminHelper.ViewModel
+{
+    public class NonModalConfirmVM : ViewModelBase
+    {
+        private readonly NonModalConfirmM nonModalConfirmM;
+
+        public NonModalConfirmVM(string question, string title = "")
+        {
+            nonModalConfirmM = new NonModalConfirmM(question, title);
+        }
+
+        public string Title { get => nonModalConfirmM.Title; }
+        public string Question { get => nonModalConfirmM.Question; }
+        public bool IsAccepted
+        {
+            get => nonModalConfirmM.IsAccepted;
+        }
+
+        public void Accept()
+        {
+            nonModalConfirmM.Accept();
+        }
+
+        public void Cancel()
+        {
+            nonModalConfirmM.Cancel();
+        }
+    }
+}
