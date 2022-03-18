aa5f22f6e296aa5c3f54252b69eb6ed1423db300
storm-sergey <st.sergey.a@gmail.com>
2022-03-18 19:45:47 +0300

    ref: codestyle

---
+++ View/NonModalConfirm.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using AdminHelper.ViewModel;
+
+
+namespace AdminHelper.View
+{
+    public partial class NonModalConfirm : Window
+    {
+        private readonly NonModalConfirmVM nonModalConfirmVM;
+        private readonly Action UnblockMainWindow;
+        private readonly Action AcceptHandler;
+
+        public NonModalConfirm(Action unblockMainWindow, Action acceptHandler, string question)
+        {
+            UnblockMainWindow = unblockMainWindow;
+            AcceptHandler = acceptHandler;
+            InitializeComponent();
+            nonModalConfirmVM = new NonModalConfirmVM(question);
+            NonModalConfirmText.Text = nonModalConfirmVM.Question;
+            NonModalConfirmWindow.Title = nonModalConfirmVM.Title;
+        }
+
+        private void NonModalMessage_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            UnblockMainWindow();
+            if (nonModalConfirmVM.IsAccepted)
+            {
+                AcceptHandler();
+            }
+        }
+
+        private void Button_Click_Accept(object sender, RoutedEventArgs e)
+        {
+            nonModalConfirmVM.Accept();
+            Close();
+        }
+
+        private void Button_Click_Cancel(object sender, RoutedEventArgs e)
+        {
+            nonModalConfirmVM.Cancel();
+            Close();
+        }
+    }
+}
